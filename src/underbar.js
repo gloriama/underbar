630b7902b78edb7c6eecd1c85d71696d440973a8
Gloria Ma <gloria.ma@aya.yale.edu>
2015-10-01 19:41:10 -0700

    Completed defaults

--- src/underbar.js
+++ src/underbar.js
@@ -234,6 +234,14 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(extension) {
+      _.each(extension, function(value, key) {
+        if (!(key in obj)) {
+          obj[key] = value;
+        }
+      });
+    });
+    return obj;
   };
 
 
