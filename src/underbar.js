64b2d2543244d716bc93580963b2f91f48c3c511
Gloria Ma <gloria.ma@aya.yale.edu>
2015-10-11 17:47:14 -0700

    Completed invoke

--- src/underbar.js
+++ src/underbar.js
@@ -350,6 +350,11 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item, i) {
+      var fn = typeof functionOrKey === "function" ?
+               functionOrKey : collection[i][functionOrKey];
+      return fn.apply(item, args);
+    });
   };
 
   // Sort the object's values by a criterion produced by an iterator.
